854eddb21fd1ca1d6381c82dc6e860f18fc42ba7
BuildTools <32042818+sideberns@users.noreply.github.com>
2020-03-12 00:39:04 -0400

    Add comments

--- Raspberry Pi/flask_smarthome.py
+++ Raspberry Pi/flask_smarthome.py
@@ -1,3 +1,4 @@
+# Import Libraries
 from flask import Flask, request, jsonify, render_template
 from time import sleep
 import sys
@@ -6,9 +7,10 @@ import serial
 # Global Variables
 WRITE_TIMES = 5
 
-app = Flask(__name__)
+app = Flask(__name__) # Create Flask object
 ser = serial.Serial('COM6', 9600) # Establish connnection to Arduino
 
+# Main Website
 @app.route("/", methods=['GET','POST'])
 def home():
     if request.method == 'POST':
@@ -19,7 +21,9 @@ def home():
         elif request.form.get('intruder_alert') == 'Intruder Alert':
             char = str.encode('i')
             print("Intruder Alert Toggled")
-            for i in range(WRITE_TIMES): ser.write(char + i - i) 
+            for i in range(WRITE_TIMES): ser.write(char + i - i) # Send char to Arduino
     return render_template('index.html')
+
+# Start Flask Server
 if __name__ == "__main__":
     app.run(debug=False)
