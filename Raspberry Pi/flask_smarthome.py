ee6d16e5054299b375122138f1def767cb9e2746
BuildTools <unconfigured@null.spigotmc.org>
2020-03-12 00:08:24 -0400

    Add basic Arduino control from webpage

--- Raspberry Pi/flask_smarthome.py
+++ Raspberry Pi/flask_smarthome.py
@@ -1,34 +1,25 @@
-from flask import Flask, render_template_string, request   # Importing the Flask modules 
-from time import sleep      # Import sleep module from time library 
-app = Flask(__name__)
-#HTML Code 
-TPL = '''
-<html>
-    <head><title>Web Page Controlled Servo</title></head>
-    <body>
-    <h2> Web Page to Control Servo</h2>
-        <form method="POST" action="test">
-            <h3> Use the slider to rotate servo  </h3>
-            <p>Slider   <input type="range" min="1" max="12.5" name="slider" /> </p>
-            <input type="submit" value="submit" />
-            <p> Button <button type="button name "ss">Toggle Smart Switch</button>
-        </form>
-    </body>
-</html>
+from flask import Flask, request, jsonify, render_template
+from time import sleep
+import sys
+import serial
 
-'''
-@app.route("/")
-def home():                                                                                                                                                         
-    return render_template_string(TPL)                        
-@app.route("/test", methods=["POST"])
-def test():
-    # Get variable Values
-    slider = request.form["slider"]
-    button = request.form["ss"]
-    print(slider)
-    print(button)
-    sleep(1)
-    return render_template_string(TPL)
-# Run the app on the local development server
+# Global Variables
+WRITE_TIMES = 5
+
+app = Flask(__name__)
+ser = serial.Serial('COM6', 9600) # Establish connnection to Arduino
+
+@app.route("/", methods=['GET','POST'])
+def home():
+    if request.method == 'POST':
+        if request.form.get('smart_switch') == 'Smart Switch':
+            char = str.encode('s')
+            print("Smart Switch Toggled")
+            for i in range(WRITE_TIMES): ser.write(char) # Send char to Arduino
+        elif request.form.get('intruder_alert') == 'Intruder Alert':
+            char = str.encode('i')
+            print("Intruder Alert Toggled")
+            for i in range(WRITE_TIMES): ser.write(char) 
+    return render_template('index.html')
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=False)
