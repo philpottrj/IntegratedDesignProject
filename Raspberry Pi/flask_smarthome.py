4121f4d86f6752938ba5a49111428ba84f704561
BuildTools <32042818+sideberns@users.noreply.github.com>
2020-03-14 10:43:19 -0400

    Add exception for Arduino-less webpage

--- Raspberry Pi/flask_smarthome.py
+++ Raspberry Pi/flask_smarthome.py
@@ -7,40 +7,47 @@ import serial
 
 # Global Variables
 WRITE_TIMES = 5
-nl = ac = ss = intruder = False
+SERIAL_OPEN = True
+nl = ac = ss = intruder = 0
 temp = 70
 
 app = Flask(__name__) # Create Flask object
-ser = serial.Serial('COM6', 9600) # Establish connection to Arduino
-ser.flushInput()
+try:
+    ser = serial.Serial('COM6', 9600) # Establish connection to Arduino
+    ser.flushInput()
+except:
+    SERIAL_OPEN = False
+    print("ERROR: Serial Port Not Open/Connected.")
 
 # Main Website
 @app.route("/", methods=['GET','POST'])
 def home():
-    ser.flushInput()
-    ser.flushOutput()
-    if request.method == 'POST':
-        if request.form.get('smart_switch') == 'Smart Switch':
-            char = str.encode('s')
-            for _ in range(WRITE_TIMES): ser.write(char) # Send char to Arduino
-        elif request.form.get('intruder_alert') == 'Intruder Alert':
-            char = str.encode('i')
-            for _ in range(WRITE_TIMES): ser.write(char) # Send char to Arduino
-    sleep(0.03)
-    ser_bytes = ser.readline()
-    byteline = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8")).split(',')
-    print(byteline)
-    while len(byteline) != 5:
-        print("ERROR ^")
-        sleep(0.01)
+    global nl, ac, ss, temp, intruder
+    if SERIAL_OPEN:
+        ser.flushInput()
+        ser.flushOutput()
+        if request.method == 'POST':
+            if request.form.get('smart_switch') == 'Smart Switch':
+                char = str.encode('s')
+                for _ in range(WRITE_TIMES): ser.write(char) # Send char to Arduino
+            elif request.form.get('intruder_alert') == 'Intruder Alert':
+                char = str.encode('i')
+                for _ in range(WRITE_TIMES): ser.write(char) # Send char to Arduino
+        sleep(0.03)
         ser_bytes = ser.readline()
         byteline = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8")).split(',')
-        print(str(byteline))
-    nl = int(byteline[0])
-    ac = int(byteline[1])
-    ss = int(byteline[2])
-    temp = int(byteline[3])
-    intruder = int(byteline[4])
+        print(byteline)
+        while len(byteline) != 5:
+            print("ERROR ^")
+            sleep(0.01)
+            ser_bytes = ser.readline()
+            byteline = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8")).split(',')
+            print(str(byteline))
+        nl = int(byteline[0])
+        ac = int(byteline[1])
+        ss = int(byteline[2])
+        temp = int(byteline[3])
+        intruder = int(byteline[4])
     return render_template('index.html', nl=nl, ac=ac, ss=ss, temp=temp, intruder=intruder)
 
 
