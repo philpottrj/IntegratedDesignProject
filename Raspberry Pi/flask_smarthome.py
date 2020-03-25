26e9fd067f74165675950b02f41943a65e6d165d
sideberns <32042818+sideberns@users.noreply.github.com>
2020-03-25 10:51:47 -0400

    Clean code, add comments

--- Raspberry Pi/flask_smarthome.py
+++ Raspberry Pi/flask_smarthome.py
@@ -1,8 +1,5 @@
 # Import Libraries
-from flask import Flask, request, jsonify, render_template
-import time
-from time import sleep
-import sys
+from flask import Flask, request, render_template
 import serial
 
 # Global Variables
@@ -13,9 +10,8 @@ DEBUG = False
 app = Flask(__name__) # Create Flask object
 try:
     ser = serial.Serial('COM6', 9600) # Establish connection to Arduino
-    ser.flushInput()
 except:
-    SERIAL_OPEN = False
+    SERIAL_OPEN = False # If no Arduino detected, still run flask webpage
     print("\n\t######################################")
     print("\tERROR: Serial Port Not Open/Connected.")
     print("\t       Running without Arduino...")
@@ -25,31 +21,32 @@ except:
 @app.route("/", methods=['GET','POST'])
 def home():
     if SERIAL_OPEN:
-        ser.flushOutput()
-        ser.flushInput()
-        if request.method == 'POST':
+        ser.flushOutput() # Clean output (prevents overloading of serial port)
+        ser.flushInput() # Clean input
+        if request.method == 'POST': # If button pressed
             if request.form.get('smart_switch') == 'Smart Switch':
                 char = str.encode('s')
                 for _ in range(WRITE_TIMES): ser.write(char) # Send char to Arduino
             elif request.form.get('intruder_alert') == 'Intruder Alert':
                 char = str.encode('i')
                 for _ in range(WRITE_TIMES): ser.write(char) # Send char to Arduino
-    return render_template('index.html')
+    return render_template('index.html') # Returns the website
 
 @app.route("/data/")
-def get_serial():
-    ser.flushOutput()
-    ser.flushInput()
-    ser_bytes = ser.readline()
-    byteline = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8")).split(',')
-    if DEBUG: print(byteline)
-    while len(byteline) != 5:
-        if DEBUG: print("ERROR ^")
-        sleep(0.01)
-        ser_bytes = ser.readline()
-        byteline = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8")).split(',')
-        if DEBUG: print(str(byteline))
-    return str([int(i) for i in byteline])[1:-1]
+def get_serial(): # Prints Arduino status to /data webpage
+    if SERIAL_OPEN:
+        ser.flushOutput() # Flush output
+        ser.flushInput() # Flush input
+        ser_bytes = ser.readline() # Read status of Arduino
+        byteline = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8")).split(',') # Convert to list
+        if DEBUG: print(byteline)
+        while len(byteline) != 5 or len(byteline[0]) == 0: # While error detected in list
+            if DEBUG: print("--------- ERROR ----------")
+            ser_bytes = ser.readline()
+            byteline = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8")).split(',')
+            if DEBUG: print(byteline)
+        return str([int(i) for i in byteline])[1:-1] # Return list to /data
+    else: return "0, 0, 0, 0, 0" # If Arduino not connected, return dummy values
     
 
 # Start Flask Server
--- Raspberry Pi/static/script.js
+++ Raspberry Pi/static/script.js
@@ -1,23 +1,28 @@
+// Function that updates values in HTML
 function updateValues() {
+    // Create an AJAX request
     $.ajax({
-        async: false,
-        type: "GET",
-        url: "/data",
-        complete: function(data) { 
-            var array = String(data.responseText).split(', ');
+        type: "GET",  // GET request (as opposed to "POST")
+        url: "/data", // Go to /data URL
+        complete: function(data) { // When AJAX request successfully completes
+            var array = String(data.responseText).split(', '); // Returns /data page and converts to array
+            // Assign array to variables
             var nl = Boolean(Number(array[0]));
             var ac = Boolean(Number(array[1]));
             var ss = Boolean(Number(array[2]));
             var temp = Number(array[3]);
             var intruder = Boolean(Number(array[4]));
-            document.getElementById('nightLight').innerHTML = "Night Light: " + nl;
-            document.getElementById('airConditioning').innerHTML = "Air Conditioning: " + ac;
-            document.getElementById('smartSwitch').innerHTML = "Smart Switch: " + ss;
-            document.getElementById('temperature').innerHTML = "Temperature: " + temp + "F";
-            document.getElementById('intruderAlert').innerHTML = "Intruder: " + intruder;
+            // Insert variables into HTML
+            document.getElementById('nightLight').innerHTML = nl;
+            document.getElementById('airConditioning').innerHTML = ac;
+            document.getElementById('smartSwitch').innerHTML = ss;
+            document.getElementById('temperature').innerHTML = temp + "F";
+            document.getElementById('intruderAlert').innerHTML = intruder;
         }
     });
 }
 
+// Execute updateValues() on load of webpage
 $(document).ready(updateValues())
+// Execute updateValues() every 300 milliseconds after webpage is loaded
 setInterval(updateValues, 300);
