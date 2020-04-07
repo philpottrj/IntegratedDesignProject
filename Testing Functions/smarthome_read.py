82cab70ff68343c884218be35c50e01c9e5fe7d9
sideberns <32042818+sideberns@users.noreply.github.com>
2020-04-07 16:01:17 -0400

    Delete testing functions

--- Testing Functions/smarthome_read.py
+++
@@ -1,9 +0,0 @@
-import serial
-from time import sleep
-ser = serial.Serial('COM6', 9600)
-ser.flushInput()
-
-while True:
-    ser_bytes = ser.readline()
-    decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
-    print(decoded_bytes)
