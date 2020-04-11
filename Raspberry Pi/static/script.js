54d00c7a15e72ec950ff92fd7f26fca6e9d60f1e
sideberns <32042818+sideberns@users.noreply.github.com>
2020-04-10 23:54:19 -0400

    Add grid to website status page

--- Raspberry Pi/static/script.js
+++ Raspberry Pi/static/script.js
@@ -13,11 +13,21 @@ function updateValues() {
             var temp = Number(array[3]);
             var intruder = boolToState(Boolean(Number(array[4])));
             // Insert variables into HTML
+            // TODO: Split code up by element instead of by action
             document.getElementById('nightLight').innerHTML = nl;
             document.getElementById('airConditioning').innerHTML = ac;
             document.getElementById('smartSwitch').innerHTML = ss;
             document.getElementById('temperature').innerHTML = temp + "F";
             document.getElementById('intruderAlert').innerHTML = intruder;
+            // Change Element colors in HTML
+            document.getElementById('nightLight-icon').style.color = updateIcons('nl',nl);
+            document.getElementById('airConditioning-icon').style.color = updateIcons('ac',ac);
+            document.getElementById('smartSwitch-icon').style.color = updateIcons('ss',ss);
+            document.getElementById('temperature-icon').style.color = updateIcons('temp',temp);
+            document.getElementById('intruderAlert-icon').style.color = updateIcons('ia',intruder);
+            // Update all icons
+            updateAC(ac);
+            updateSS(ss);
         }
     });
 }
@@ -39,7 +49,104 @@ function boolToState(bool) {
     return state;
 }
 
+function updateAC(state) {
+    if (state == "On") {
+        $('#airConditioning-icon').addClass('fa-spin');
+    }
+    else {
+        $('#airConditioning-icon').removeClass('fa-spin');
+    }
+}
+
+function updateSS(state) {
+    if (state == "On") {
+        $('#smartSwitch-icon').removeClass('fa-toggle-off').addClass('fa-toggle-on');
+    }
+    else {
+        $('#smartSwitch-icon').removeClass('fa-toggle-on').addClass('fa-toggle-off');
+    }
+}
+
+function updateTemp(state) {
+    if (isNaN(state) || state == 0) {
+        color = colors.disabled;
+    }
+    else if (state < 70) {
+        color = colors.green;
+    } 
+    else if (state < 80) {
+        color = colors.yellow;
+    }
+    else {
+        color = colors.red;
+    }
+    break;
+}
+
+function updateIcons(icon,state) {
+    let colors = {
+        red: "#DC3545", // old red: "#C23127",
+        yellow: "#FFC107", // old yellow: "#FFCC02",
+        green: "#28A745", // old green: "#4BD964",
+        disabled: "#6C757D", // old disabled: "#CCCCCC",
+        error: "#E83E8C" // old error: "#FF0080"
+    }
+    let color = "";
+    switch (icon) {
+        case 'nl':
+            if (state == "On") {
+                color = colors.yellow;
+            } 
+            else {
+                color = colors.disabled;
+            }
+            break;
+        case 'ac':
+            if (state == "On") {
+                color = colors.green;
+            } 
+            else {
+                color = colors.disabled;
+            }
+            break;
+        case 'ss':
+            if (state == "On") {
+                color = colors.green;
+            } 
+            else {
+                color = colors.disabled;
+            }
+            break;
+        case 'temp':
+            if (isNaN(state) || state == 0) {
+                color = colors.disabled;
+            }
+            else if (state < 70) {
+                color = colors.green;
+            } 
+            else if (state < 80) {
+                color = colors.yellow;
+            }
+            else {
+                color = colors.red;
+            }
+            break;
+        case 'ia':
+            if (state == "On") {
+                color = colors.red;
+            } 
+            else {
+                color = colors.disabled;
+            }
+            break;
+        default:
+            color = color.error;
+            break;
+    }
+    return color;
+}
+
 // Execute updateValues() on load of webpage
 $(document).ready(updateValues())
 // Execute updateValues() every 300 milliseconds after webpage is loaded
-setInterval(updateValues, 2000);
+setInterval(updateValues, 1000);
